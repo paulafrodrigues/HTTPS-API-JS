12c4b621f64b941f57fbb4c207d89e94c43d57bf
paulafrodrigues <pfr.blackcat@gmail.com>
2019-10-15 22:18:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const server =  express();
+
+server.get('/', function(req, res){
+    res.send('I love you 3000!');
+});
+
+server.get('/', function(req, res){
+    res.send('root');
+});
+
+server.get('/about', function(req, res){
+    res.send('about');
+});
+
+server.get('/users/:username', (req, res, next) => {
+    res.send(req.params);
+});
+
+server.get('/books/:bookId', (req, res, next) => {
+    res.send(req.params);
+});
+
+server.listen(3000);
